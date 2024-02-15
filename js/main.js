cff82ae8b77e052657d2162674e84c792f4d410b
TrueWizardia <kirkozlov7913@mail.ru>
2024-02-15 11:06:19 +0700

    all without storage

--- js/main.js
+++ js/main.js
@@ -5,42 +5,29 @@ let app = new Vue({
         done_blocker: false,
         add_blocker: false,
         tasks: [
-            {
-                column: 1,
-                time_stamp: null,
-                task_list: [
-                    {
-                        title: 1,
-                        done: false
-                    },
-                    {
-                        title: 2,
-                        done: false
-                    },
-                    {
-                        title: 3,
-                        done: false
-                    },
-                    {
-                        title: 4,
-                        done: false
-                    },
-                ]
-            },
-            {
-                column: 1,
-                time_stamp: null,
-                task_list: [
-                    {
-                        title: 1,
-                        done: false
-                    },
-                    {
-                        t: 1,
-                        a: 1
-                    }
-                ]
-            }
+            // {
+            //     column: 1,
+            //     time_stamp: null,
+            //     task_list: [
+            //         {
+            //             title: 1,
+            //             done: false
+            //         },
+            //         {
+            //             title: 2,
+            //             done: false
+            //         },
+            //         {
+            //             title: 3,
+            //             done: false
+            //         },
+            //         {
+            //             title: 4,
+            //             done: false
+            //         },
+            //     ]
+            // },
+
 
         ],
 
@@ -71,12 +58,16 @@ let app = new Vue({
 
                     task.column = 3
 
-                    task.time_stamp= "Время"+ new Date(Date.now()).getHours()+new Date(Date.now()).getMinutes()+ 'Дата'+new Date(Date.now()).getFullYear()
+                    task.time_stamp= "Время "+ new Date(Date.now()).getHours()+":"+new Date(Date.now()).getMinutes()+
+                        ' Дата '+new Date(Date.now()).getDate()+":"+(new Date(Date.now()).getMonth()+1)+":"+new Date(Date.now()).getFullYear()
+
                     if (this.done_blocker === true) {
                         this.done_blocker = false
-                        for (askTask in this.tasks) {
-                            if (askTask.column === 1) {
-                                this.toNextColumn(askTask)
+
+                        for (i in this.tasks) {
+
+                            if (this.tasks[i].column === 1) {
+                                this.toNextColumn(this.tasks[i])
                             }
                         }
 
@@ -102,13 +93,15 @@ let app = new Vue({
                         if (this.tasks[i].column === 2) {
                             col_checker++
                         }
-                        if (col_checker === 5) {
+                        if (col_checker >= 5) {
                             this.done_blocker = true
                             break
                         }
                     }
                     if (this.done_blocker === false) {
+
                         task.column = 2
+                        this.checker_add()
                     }
                 }
             }
@@ -116,8 +109,10 @@ let app = new Vue({
         checker_add() {
             count = 0;
             for (i = 0; i < this.tasks.length; i++) {
-                if (this.tasks[0]["column"] == 1) {
+                if (this.tasks[i]["column"] == 1) {
+
                     count++
+                    console.log(count)
                 }
                 if (count == 3) {
                     this.add_blocker = true
